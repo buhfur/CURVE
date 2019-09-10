5fe95f21d2c904a465625082632b8caca5651784
ryanmcvicker12 <36174949+ryanmcvicker12@users.noreply.github.com>
2019-09-10 12:43:26 -0400

    added more to gun cs

--- SpaceShip/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Gun.cs
+++ SpaceShip/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Gun.cs
@@ -21,5 +21,34 @@ public class Gun : MonoBehaviour
     private Shooter myOwner;
 
     [SerializeField]
-    SOMETHING
+    private float bulletSpreadRadius;
+
+
+    public void Shoot(Vector3 aimTarget)
+    {
+        muzzleFlash.Play();
+
+        gunShot.Play();
+
+        GameObject tempBullet =
+            Instantiate(bulletPrefab, bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        tempBullet.transform.LookAt(aimTarget);
+
+        Bullet currentBullet =
+            tempBullet.GetComponent<Bullet>();
+
+        currentBullet.damage = bulletDamage;
+
+        currentBullet.owner = myOwner;
+
+
+        float bulletDirectionX = Random.Range(-bulletSpreadRadius, bulletSpreadRadius);
+
+        float bulletDirectionY = Random.Range(-bulletSpreadRadius, bulletSpreadRadius);
+
+        currentBullet.ShootBullet(currentBullet.transform.TransformDirection(
+            new Vector3(bulletDirectionX, bulletDirectionY, 1)));
+    }
 }
